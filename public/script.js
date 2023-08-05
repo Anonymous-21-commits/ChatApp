5b5c348b1475a45efbd536d1a7bc2e474752b843
Soumalya Majumdar <soumalyamajumdar18@gmail.com>
2023-08-05 17:37:10 +0530

    the set up completed

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const socket=io();
+let btn=document.getElementById('btn');
+let inputMsg=document.getElementById('newmsg');
+let msgList=document.getElementById('msgList');
+btn.onclick=function exec(){
+    socket.emit('msg_send',{
+        msg:inputMsg.value
+    });
+}
+socket.on('msg_rcvd',(data)=>{
+    let limsg=document.createElement('li');
+    limsg.innerText=data.msg;
+    msgList.appendChild(limsg);
+})
